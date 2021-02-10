ec34c4eb0d33c65c46e4b5e09c4ea43998e5c183
Pawel <moonster1j@gmail.com>
2021-02-10 18:42:10 +0100

    Added tilemap generator

---
+++ Unity_Portfolio/Portfol.io/Assets/MapEditor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CustomEditor(typeof (MapGenerator))]
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
